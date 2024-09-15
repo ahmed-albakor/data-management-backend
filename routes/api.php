b91a98524f121083e05d0a43899d59b8999b13e6
ahmed-albakor <ahmed.albakor206@gmail.com>
2024-09-15 14:25:37 +0300

    fix auth and routes 7

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ Route::get('/user', function (Request $request) {
 Route::post('login', [AuthController::class, 'login']);
 
 // حماية الروابط بواسطة `auth:sanctum` و `AdminMiddleware`
-Route::middleware([AdminMiddleware::class, 'auth:sanctum'])->group(function () {
+Route::middleware([AdminMiddleware::class])->group(function () {
 
     // طرق خاصة بالمصادقة
     Route::post('changePassword', [AuthController::class, 'changePassword']);
